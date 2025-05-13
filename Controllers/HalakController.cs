ebff907153f7fc13a206307e80dc701d2930d626
kissagnesa <sindzse88@gmail.com>
2025-05-13 18:01:57 +0200

    HalakControllers created

---
+++ Controllers/HalakController.cs
@@ -0,0 +1,43 @@
+﻿using HalakAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HalakAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HalakController : ControllerBase
+    {
+        [HttpGet("FajMeretTo")]
+        public IActionResult GetFajMeretTo()
+        {
+            try
+            {
+               using(var cx=new HalakContext())
+                {
+                    var response = cx.Halaks
+                        .Select(x => new
+                        {
+                            x.Faj,
+                            x.MeretCm,
+                            x.To.Nev
+                        })
+                        .ToList();
+                    return Ok(response);
+                }
+
+                /* VAAGY
+                 var response = cx.Halaks.Include(x => x.To).Select(f=>new FajMeretTo{Faj=f.Faj,
+                     MeretCm=f.MeretCm,
+                     ToNev=f.To.Nev
+                 }).ToList();
+                    return Ok(response);                     
+                 */
+            }
+            catch (Exception)
+            {
+                return StatusCode(400);
+            }
+        }
+    }
+}
